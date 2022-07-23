25019ccad942b149900964e8413ca7279667c4f6
kolpaland <kolpaland@mail.ru>
2022-07-23 18:21:05 +0700

    add handlebars

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 
 import {sum} from './modules/sum';
+import tpl from './../index.hbs';
+
+document.body.innerHTML = tpl({id: "root"});
 
 const root = document.querySelector('#root');
 root.textContent = sum(6, -1).toString(); 
