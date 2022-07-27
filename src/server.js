7211c91fa692968f73ebe9f05476832ecd9928fc
Черепкова Екатерина Васильевна <cherepkova_ev@lemz-t.ru>
2022-07-27 14:57:19 +0700

    fix server

--- src/server.js
+++ src/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
-const PORT = 3000;
 
-app.use(express.static('./dist/'));
+app.use(express.static(path.join(__dirname, './../dist')));
 
-app.listen(PORT, function () {
-  console.log(`Example app listening on port ${PORT}!`);
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './../dist/index.html'));
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
