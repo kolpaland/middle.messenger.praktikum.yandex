058fa91e032a009333ab0c5b67a54ba7aa08fb50
kolpaland <kolpaland@mail.ru>
2022-08-11 00:25:24 +0700

    add chat page

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -1,14 +1,8 @@
 import PageChat from './chat.hbs';
-import PageStub from './../../layout/stub/stub';
+import * as ellipce from './../../../static/images/profile/Ellipse.png'
 
 import './chat.scss';
 
 export default PageChat({
-
-    stub: PageStub({
-        rout : "/login",
-        routText : "Войти",
-        title: "Чат",
-        text: "Чат еще не готов"
-    })
+    ellipce
 });
